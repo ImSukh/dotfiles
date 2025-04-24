42e30d590941402972ba44eb6500cc4f11f618f9
ImSukh <therealsukhjit@gmail.com>
2025-04-24 19:16:46 -0400

    add printenv back in

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 fi
 echo "Figuring out which code we are running"
+printenv
 which code
 code -v
 # Instead of changing the default shell, set up Bash to automatically start zsh
